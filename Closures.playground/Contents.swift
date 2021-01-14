6abe4edcf5813fd1c43eca982c18a7506b9407cc
Nicole Liu <nicoleliu_@outlook.com>
2021-01-14 08:59:22 -0500

    Finished first two closure videos to learn about creating closures and accepting parameters in them

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -1,1 +1,18 @@
 import Cocoa
+// Creating Basic Closures
+
+// functions who are assigned to a variable, called using the variable, and passed into other functions as parameters
+
+//let driving = {
+//    print("I'm driving in my car")
+//}
+//
+//driving ()
+
+// Accepting parameters in a closure
+
+let driving = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+driving("London")
+
