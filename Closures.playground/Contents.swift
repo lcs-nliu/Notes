0d3899cdd3c5fc7f644008617fdcd282ae2e1c70
Nicole Liu <nicoleliu_@outlook.com>
2021-01-17 23:33:38 -0500

    Finished basic closures pt 2 exercises

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -42,13 +42,94 @@ print(message)
 
 // Trailing closure syntax
 
-func travel(action: () -> Void) {
+//func travel(action: () -> Void) {
+//    print("I'm getting ready to go.")
+//    action()
+//    print("I arrived!")
+//}
+//
+//travel {
+//    print("I'm driving in my car")
+//}
+
+// CLOSURE PART TWO
+
+// Using closures as parameters
+
+func travel(action: (String) -> Void) {
     print("I'm getting ready to go.")
-    action()
+    action("London")
     print("I arrived!")
 }
 
-travel {
-    print("I'm driving in my car")
+travel { (place: String) in
+    print("I'm going to \(place) in my car")
 }
+
+
+
+
+func makePizza(addToppings: (Int) -> Void) {
+    print("The dough is ready.")
+    print("The base is flat.")
+    addToppings(2)
+}
+makePizza { (toppingCount: Int) in
+    let toppings = ["ham", "salami", "onions", "peppers"]
+    for i in 0...toppingCount {
+        let topping = toppings[i]
+        print("I'm adding \(topping)")
+    }
+}
+
+// Using closures as parameters when they return values
+
+//func travel(action: (String) -> String) {
+//    print("I'm getting ready to go.")
+//    let description = action("London")
+//    print(description)
+//    print("I arrived!")
+//}
+//
+//travel { (place: String) -> String in
+//    return "I'm going to \(place) in my car"
+//}
+
+// Shorthand parameter names
+// rewriting the function/closure above in shorthand
+
+
+func travel(action: (String) -> String) {
+    print("I'm getting ready to go.")
+    let description = action("London")
+    print(description)
+    print("I arrived!")
+}
+
+// Simplify 1
+
+//travel { place -> String in
+//    return "I'm going to \(place) in my car"
+//}
+
+
+// Simplify 2
+
+//travel { place in
+//    return "I'm going to \(place) in my car"
+//}
+
+// Simplify 3
+
+//travel { place in
+//    "I'm going to \(place) in my car"
+//}
+
+// Simplify 4
+
+travel {
+    "I'm going to \($0) in my car"
+}
+
+
 
