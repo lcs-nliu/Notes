7e7120390813fa991a9f092d2ad838b1fdaaf062
Nicole Liu <nicoleliu_@outlook.com>
2021-01-18 23:31:16 -0500

    Finished structures exercises (including methods, properties of arrays, and properties of strings)

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -45,4 +45,49 @@ progress.amount = 30
 progress.amount = 80
 progress.amount = 100
 
+// Methods
 
+
+struct City {
+    var population: Int
+
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+
+let london = City(population: 9_000_000)
+print(london.collectTaxes())
+
+
+// Mutating methods
+
+struct Person {
+    var name: String
+
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+
+// Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+print(string.count)
+print(string.hasPrefix("Do"))
+print(string.uppercased())
+print(string.sorted())
+
+// Properties and methods of arrays
+
+var toys = ["Woody"]
+print(toys.count)
+toys.append("Buzz")
+toys.firstIndex(of: "Buzz")
+print(toys.sorted())
+toys.remove(at: 0)
+print(toys)
